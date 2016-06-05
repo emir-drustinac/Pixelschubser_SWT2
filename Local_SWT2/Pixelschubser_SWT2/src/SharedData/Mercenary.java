26081ee85492d4088c189cdca2109d4bdb5e5248
alexanderbocko <alexanderbocko@stud.fh-dortmund.de>
2016-06-05 13:39:59 +0200

    added to Mercenary  isDefendingProkonsul()

--- Local_SWT2/Pixelschubser_SWT2/src/SharedData/Mercenary.java
+++ Local_SWT2/Pixelschubser_SWT2/src/SharedData/Mercenary.java
@@ -3,5 +3,26 @@ package SharedData;
 public class Mercenary {
 
 	private String target;
+	private boolean defendingProconsul;		// to separate defending merc from offending one on Proconsul
+	
+	public Mercenary() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getTarget() {
+		return target;
+	}
+
+	public void setTarget(String target) {
+		this.target = target;
+	}
+
+	public boolean isDefendingProconsul() {
+		return defendingProconsul;
+	}
+
+	public void setDefendingProconsul(boolean defendingProconsul) {
+		this.defendingProconsul = defendingProconsul;
+	}
 
 }
