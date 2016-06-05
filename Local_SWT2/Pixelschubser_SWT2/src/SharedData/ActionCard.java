8883e23f947386d06576768a4b9b49f192e2066e
alexanderbocko <alexanderbocko@stud.fh-dortmund.de>
2016-06-05 14:44:43 +0200

    added All Cards wenn die Karten shon sowieso ENUM sind, vlt die ganzen Methoden streichen und Logic entscheidenlassen was mit welcher Karte tun? Man könnte die noch in 3 klassen unterteilen ActionCards, BuyCards, VictoryCards

--- Local_SWT2/Pixelschubser_SWT2/src/SharedData/ActionCard.java
+++ Local_SWT2/Pixelschubser_SWT2/src/SharedData/ActionCard.java
@@ -1,41 +1,99 @@
 package SharedData;
 
 public class ActionCard {
+	
+	/*
+	 * PromisesCard (After the Phase)
+	 * 		# Picklock (Einbrecher)
+	 * 
+	 * CommandMercenariesCard (After the Phase)
+	 * 		# Spy
+	 * 
+	 * Combatcards
+	 * 		Defender
+	 * 			# Assassination
+	 * 			# Catapult
+	 * 			# Slave Revolt
+	 * 			# Bribe
+	 * 			# Lion
+	 * 
+	 * 		Offender (incl. Defender Cards)
+	 * 			# Surprise Attack
+	 * 			# Annexation
+	 * 
+	 * 		Dice
+	 * 			# Abuse of Power??? (Machtmissbrauch/Ablenkungsmanöver)
+	 * 
+	 * SpendMoneyCards
+	 * 		MoneyCards
+	 * 			# Denari1000
+	 * 			# Denari2000
+	 * 			# Denari3000
+	 * 		FreeBuyCards
+	 * 			# Propaganda (free Mercenary)
+	 * 			# Free Buiding
+	 * 
+	 * VictoryPintsCards
+	 * 		# Golden Statue
+	 * 		# Golden Chariot
+	 * 
+	 * NothingCard
+	 * 		# Entertainer? Juggler? (Gauklertruppe)
+	 * 
+	 */
 
 	public enum CardType {
-		Money1000, Money2000, Money3000
-		// TODO: add other types
+		// nach Phasen sortiert
+		PICKLOCK, 
+		SPY, 
+		ASSASSINATION, CATAPULT, SLAVEREVOLT, BRIBE, LION,
+		SURPRISEATTACK, ANNEXATION,
+		ABUSEOFPOWER,
+		DENARI1000, DENARI2000, DENARI3000,
+		PROPAGANDA, FREEBUILDING,
+		JUGGLER
 	}
 	private CardType type;
-
-	public boolean addsPoint() {
-		// TODO - implement ActionCard.addsPoint
-		throw new UnsupportedOperationException();
-	}
-
-	public boolean usableDuringFight() {
-		// TODO - implement ActionCard.usableDuringFight
-		throw new UnsupportedOperationException();
-	}
+	
+	/* 
+	 * TODO: #### wenn die Karten shon sowieso ENUM sind, 
+	 * vlt die ganzen Methoden streichen und Logic entscheidenlassen was mit welcher Karte tun?
+	 * 
+	 */
+	
+//	public boolean addsPoint() {
+//		// TODO - implement ActionCard.addsPoint
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	public boolean usableDuringFight() {
+//		// TODO - implement ActionCard.usableDuringFight
+//		throw new UnsupportedOperationException();
+//	}
+//	
+//	public boolean usableForOffender(){
+//		// TODO
+//		return false;
+//	}
 
 	public CardType getType() {
 		return this.type;
 	}
 
-	public String getName() {
-		// TODO - implement ActionCard.getName
-		throw new UnsupportedOperationException();
-	}
+//	public String getName() {
+//		// TODO - implement ActionCard.getName
+//		throw new UnsupportedOperationException();
+//	}
 
-	public boolean isMoneyCard() {
-		return type == CardType.Money1000 || type == CardType.Money2000 || type == CardType.Money3000;
-	}
-
-	public int moneyValue() {
-		if (type == CardType.Money1000) return 1000;
-		if (type == CardType.Money2000) return 2000;
-		if (type == CardType.Money3000) return 3000;
-		return 0;
-	}
+//	public boolean isMoneyCard() {
+//		return type == CardType.DENARI1000 || type == CardType.DENARI2000 || type == CardType.DENARI3000;
+//	}
+//
+//	public int moneyValue() {
+//		if (type == CardType.DENARI1000) return 1000;
+//		if (type == CardType.DENARI2000) return 2000;
+//		if (type == CardType.DENARI3000) return 3000;
+//		return 0;
+//	}
 
 }
