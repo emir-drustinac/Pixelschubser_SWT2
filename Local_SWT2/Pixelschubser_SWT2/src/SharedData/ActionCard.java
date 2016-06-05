f8443aa9d39bb53537ab768c54c247c9cbf37c50
Christian Freimark <freimark@users.noreply.github.com>
2016-06-05 18:05:29 +0200

    added ActionCard names

--- Local_SWT2/Pixelschubser_SWT2/src/SharedData/ActionCard.java
+++ Local_SWT2/Pixelschubser_SWT2/src/SharedData/ActionCard.java
@@ -22,7 +22,7 @@ public class ActionCard {
 	 * 			# Annexation
 	 * 
 	 * 		Dice
-	 * 			# Abuse of Power??? (Machtmissbrauch/AblenkungsmanÃ¶ver)
+	 * 			# Abuse of Power??? (Machtmissbrauch/Ablenkungsmanöver)
 	 * 
 	 * SpendMoneyCards
 	 * 		MoneyCards
@@ -31,7 +31,7 @@ public class ActionCard {
 	 * 			# Denari3000
 	 * 		FreeBuyCards
 	 * 			# Propaganda (free Mercenary)
-	 * 			# Free Buiding
+	 * 			# Free Building
 	 * 
 	 * VictoryPintsCards
 	 * 		# Golden Statue
@@ -43,22 +43,40 @@ public class ActionCard {
 	 */
 
 	public enum CardType {
-		// nach Phasen sortiert
-		PICKLOCK, 
-		SPY, 
-		ASSASSINATION, CATAPULT, SLAVEREVOLT, BRIBE, LION,
-		SURPRISEATTACK, ANNEXATION,
-		ABUSEOFPOWER,
+		/* welche Phase? */
+		PICKLOCK, // Einbrecher - darf wann gespielt werden? - bewirkt was?
+		/* Kampfplanungsphase */
+		SPY, // ? - ? - ?
+		/* Kampfphase */
+		// Offender
+		ASSASSINATION, // ? - ? - ?
+		CATAPULT, // ? - ? - ?
+		SLAVEREVOLT, // ? - ? - ?
+		BRIBE, // ? - ? - ?
+		LION, // ? - ? - ?
+		// Defender
+		SURPRISEATTACK, // ? - ? - ?
+		ANNEXATION, // ? - ? - ?
+		// 
+		ABUSEOFPOWER, // Machtmissbrauch - ? - ? 
+		/* Geld ausgeben Phase */
+		// Geldkarten
 		DENARI1000, DENARI2000, DENARI3000,
-		PROPAGANDA, FREEBUILDING,
-		JUGGLER
+		// 
+		PROPAGANDA, // Propaganda - ? - ?
+		FREEBUILDING, // ? - ? - ?
+		// Extra Point Cards
+		GOLDENLION, // Goldener Löwe - kann nicht ausgespielt werden - gibt einen extra Machtpunkt
+		GOLDENCHARIOT, // Goldener Wagen - kann nicht ausgespielt werden - gibt einen extra Machtpunkt
+		// Junk Karte
+		JUGGLER // Gauklertruppe - kann nicht ausgespielt werden - bewirkt nichts 
 	}
 	private CardType type;
 	
 	/* 
 	 * TODO: #### wenn die Karten shon sowieso ENUM sind, 
-	 * vlt die ganzen Methoden streichen und Logic entscheidenlassen was mit welcher Karte tun?
-	 * 
+	 * vlt die ganzen Methoden streichen und Logic entscheiden lassen was mit welcher Karte tun?
+	 * CFR: die Methoden unterstützen die Logic dabei zu entscheiden, welche Karte nutzbar ist
 	 */
 	
 	public boolean addsPoint() {
@@ -67,13 +85,37 @@ public class ActionCard {
 	}
 
 	public boolean usableDuringFight() {
-		// TODO - implement ActionCard.usableDuringFight
-		throw new UnsupportedOperationException();
+		return 
+				type == CardType.ASSASSINATION ||
+				type == CardType.CATAPULT ||
+				type == CardType.SLAVEREVOLT ||
+				type == CardType.BRIBE ||
+				type == CardType.LION ||
+				type == CardType.SURPRISEATTACK ||
+				type == CardType.ANNEXATION ||
+				type == CardType.ABUSEOFPOWER;
 	}
-	
+
 	public boolean usableForOffender(){
-		// TODO
-		return false;
+		return 
+				type == CardType.ASSASSINATION ||
+				type == CardType.CATAPULT ||
+				type == CardType.SLAVEREVOLT ||
+				type == CardType.BRIBE ||
+				type == CardType.LION ||
+				type == CardType.ABUSEOFPOWER;
+	}
+
+	public boolean usableByDefender() {
+		return 
+				type == CardType.ASSASSINATION ||
+				type == CardType.CATAPULT ||
+				type == CardType.SLAVEREVOLT ||
+				type == CardType.BRIBE ||
+				type == CardType.LION ||
+				type == CardType.SURPRISEATTACK ||
+				type == CardType.ANNEXATION ||
+				type == CardType.ABUSEOFPOWER;
 	}
 
 	public CardType getType() {
@@ -81,8 +123,27 @@ public class ActionCard {
 	}
 
 	public String getName() {
-		// TODO - implement ActionCard.getName
-		throw new UnsupportedOperationException();
+		switch (type) {
+		case PICKLOCK: return "Einbrecher";
+		case SPY: return "Spion";
+		case ASSASSINATION: return "Attentat";
+		case CATAPULT: return "Katapult";
+		case SLAVEREVOLT: return "Sklavenaufstand";
+		case BRIBE: return "Schmiergeld";
+		case LION: return "Löwe";
+		case SURPRISEATTACK: return "Überraschungsangriff";
+		case ANNEXATION: return "Annexion";
+		case ABUSEOFPOWER: return "Machtmissbrauch";
+		case DENARI1000: return "1000 Denari";
+		case DENARI2000: return "2000 Denari";
+		case DENARI3000: return "3000 Denari";
+		case PROPAGANDA: return "Propaganda";
+		case FREEBUILDING: return "Freies Gebäude";
+		case GOLDENLION: return "Goldener Löwe";
+		case GOLDENCHARIOT: return "Goldener Wagen";
+		case JUGGLER: return "Gauklertruppe";
+		}
+		return "unknown card";
 	}
 
 	public boolean isMoneyCard() {
